bf5543834a0f92b1312eb200d0033bfb41e28ef4
Nibal <niball_ayman2008@live.com>
2020-08-23 15:56:34 +0300

    Updates

---
+++ precache-manifest.74b54dda3c5480bf2b225fa1b0ca8a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e2813545aaf6b95645c9cb1ff526eb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a8d8601503541bb19e54",
+    "url": "/todo/static/css/main.9be63033.chunk.css"
+  },
+  {
+    "revision": "80647f86cb4cfdbbce8f",
+    "url": "/todo/static/js/2.60efb5b7.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/todo/static/js/2.60efb5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d8601503541bb19e54",
+    "url": "/todo/static/js/main.7462e91b.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  },
+  {
+    "revision": "a9112bf3fd989ea87a9d70c561a8f405",
+    "url": "/todo/static/media/background.a9112bf3.jpg"
+  }
+]);
